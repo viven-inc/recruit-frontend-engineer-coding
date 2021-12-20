8c0d61c643a5e9436a60882fc13db544874552ff
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2021-12-20 13:31:38 +0900

    [UPD] package

--- functionA.test.js
+++ functionA.test.js
@@ -1,8 +1,15 @@
 
 let functionA = require("./functionA")
 
-test('functionA',() => {
+test('functionA 1',() => {
+    expect(functionA(19)).toBe(19)
+})
+test('functionA 2',() => {
     expect(functionA(20)).toBe(23)
+})
+test('functionA 3',() => {
     expect(functionA(2)).toBe(2)
+})
+test('functionA 4',() => {
     expect(functionA(99992)).toBe(100003)
 })
